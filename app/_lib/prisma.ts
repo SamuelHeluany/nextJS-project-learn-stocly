043860c1537b90fe8a465591db25dc1bcb8e8317
Samuel Heluany <samuelheluany@hotmail.com>
2026-07-13 16:21:13 -0300

    feat: add products table

---
+++ app/_lib/prisma.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+declare global {
+  var cachedPrisma: ReturnType<typeof createPrismaClient>;
+}
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const createPrismaClient = () => {
+  return new PrismaClient({ adapter }).$extends({
+    result: {
+      product: {
+        status: {
+          needs: { stock: true },
+          compute(product) {
+            if (product.stock <= 0) {
+              return "OUT_OF_STOCK";
+            }
+
+            return "IN_STOCK";
+          },
+        },
+      },
+    },
+  });
+};
+
+let prisma: ReturnType<typeof createPrismaClient>;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = createPrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = createPrismaClient();
+  }
+
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
