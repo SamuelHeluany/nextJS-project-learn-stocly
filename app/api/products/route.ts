0d6e5ba339c78c6b9d7028c53aa444434bc15a03
Samuel Heluany <samuelheluany@hotmail.com>
2026-07-15 08:18:03 -0300

    chore: add comments

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
   });
 }
 
+// POST, ADICIONAR NOVO PRODUTO
 export async function POST(request: Request) {
   const body = await request.json();
   const name = body.name;
