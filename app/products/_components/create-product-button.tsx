d6c66588ca1335d87afa58cde3d13235671a543b
Samuel Heluany <samuelheluany@hotmail.com>
2026-07-23 14:55:21 -0300

    refactor: add UpsertDialogProdcutContent

--- app/products/_components/create-product-button.tsx
+++ app/products/_components/create-product-button.tsx
@@ -1,58 +1,15 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-  DialogHeader,
-  DialogFooter,
-  DialogClose,
-} from "@/app/_components/ui/dialog";
-import { Loader2Icon, PlusIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/app/_components/ui/form";
-import { Input } from "@/app/_components/ui/input";
-import { NumericFormat } from "react-number-format";
-import { createProducts } from "@/app/_actions/create-product";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import { PlusIcon } from "lucide-react";
 import { useState } from "react";
-import {
-  createProductSchema,
-  CreateProductSchema,
-} from "@/app/_actions/create-product/schema";
+import UpsertProductDialogContent from "./upsert-product-dialog";
 
 const CreateProductButton = () => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
   // Chamar o useForm do react-hook-form, passando o zodResolver para validar o formulário com o schema definido acima, e os valores padrões do formulário
-  const form = useForm<CreateProductSchema>({
-    // shouldUnregister faz com que limpe os imputs quando fechar o modal
-    shouldUnregister: true,
-    resolver: zodResolver(createProductSchema),
-    defaultValues: {
-      name: "",
-      price: 0,
-      stock: 1,
-    },
-  });
 
-  const onSubmit = async (data: CreateProductSchema) => {
-    try {
-      await createProducts(data);
-      setDialogIsOpen(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
       <DialogTrigger asChild>
@@ -61,102 +18,7 @@ const CreateProductButton = () => {
           Novo Produto
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <Form {...form}>
-          <form
-            id="form-rhf-demo"
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8"
-          >
-            <DialogHeader>
-              <DialogTitle>Criar Produto</DialogTitle>
-              <DialogDescription>
-                Insira as informações abaixo
-              </DialogDescription>
-            </DialogHeader>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome do Produto</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder="Digite o nome do produto" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Valor do produto</FormLabel>
-                  <FormControl>
-                    {/* bilioteca react-number-format */}
-                    <NumericFormat
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      fixedDecimalScale
-                      decimalScale={2}
-                      prefix="R$ "
-                      allowNegative={false}
-                      customInput={Input}
-                      //   resolver problema do formato ser uma string mas é para receber um number
-                      onValueChange={(values) =>
-                        field.onChange(values.floatValue)
-                      }
-                      {...field}
-                      onChange={() => {}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="stock"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Estoque do produto</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="number"
-                      placeholder="Digite o estoque do produto"
-                      onChange={(e) => field.onChange(Number(e.target.value))}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <DialogFooter>
-              {/* Como tem um button dentro de outro, colocar aschild */}
-              <DialogClose asChild>
-                <Button type="reset" variant="secondary">
-                  Cancelar
-                </Button>
-              </DialogClose>
-              {/* Vai ser verdadeiro enquanto a server action estiver sendo executada, ou seja, a função submit ser executada */}
-              <Button
-                type="submit"
-                disabled={form.formState.isSubmitting}
-                className="gap-1.5"
-              >
-                {/* se estiver enviando a criação do produto, coloca um spin de carregamento */}
-                {form.formState.isSubmitting && (
-                  <Loader2Icon className="animate-spin" size={16} />
-                )}
-                Salvar
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+      <UpsertProductDialogContent onSuccess={() => setDialogIsOpen(false)} />
     </Dialog>
   );
 };
---
+++ app/products/_components/upsert-product-dialog.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { createProducts } from "@/app/_actions/create-product";
+import {
+  createProductSchema,
+  CreateProductSchema,
+} from "@/app/_actions/create-product/schema";
+import { Button } from "@/app/_components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/app/_components/ui/dialog";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2Icon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { NumericFormat } from "react-number-format";
+
+interface UpsertProductDialogContentProps {
+  onSuccess: () => void;
+}
+
+const UpsertProductDialogContent = ({
+  onSuccess,
+}: UpsertProductDialogContentProps) => {
+  const form = useForm<CreateProductSchema>({
+    // shouldUnregister faz com que limpe os imputs quando fechar o modal
+    shouldUnregister: true,
+    resolver: zodResolver(createProductSchema),
+    defaultValues: {
+      name: "",
+      price: 0,
+      stock: 1,
+    },
+  });
+
+  const onSubmit = async (data: CreateProductSchema) => {
+    try {
+      await createProducts(data);
+      onSuccess?.();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <DialogContent>
+      <Form {...form}>
+        <form
+          id="form-rhf-demo"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          <DialogHeader>
+            <DialogTitle>Criar Produto</DialogTitle>
+            <DialogDescription>Insira as informações abaixo</DialogDescription>
+          </DialogHeader>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome do Produto</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Digite o nome do produto" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Valor do produto</FormLabel>
+                <FormControl>
+                  {/* bilioteca react-number-format */}
+                  <NumericFormat
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    fixedDecimalScale
+                    decimalScale={2}
+                    prefix="R$ "
+                    allowNegative={false}
+                    customInput={Input}
+                    //   resolver problema do formato ser uma string mas é para receber um number
+                    onValueChange={(values) =>
+                      field.onChange(values.floatValue)
+                    }
+                    {...field}
+                    onChange={() => {}}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="stock"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estoque do produto</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="number"
+                    placeholder="Digite o estoque do produto"
+                    onChange={(e) => field.onChange(Number(e.target.value))}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <DialogFooter>
+            {/* Como tem um button dentro de outro, colocar aschild */}
+            <DialogClose asChild>
+              <Button type="reset" variant="secondary">
+                Cancelar
+              </Button>
+            </DialogClose>
+            {/* Vai ser verdadeiro enquanto a server action estiver sendo executada, ou seja, a função submit ser executada */}
+            <Button
+              type="submit"
+              disabled={form.formState.isSubmitting}
+              className="gap-1.5"
+            >
+              {/* se estiver enviando a criação do produto, coloca um spin de carregamento */}
+              {form.formState.isSubmitting && (
+                <Loader2Icon className="animate-spin" size={16} />
+              )}
+              Salvar
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </DialogContent>
+  );
+};
+
+export default UpsertProductDialogContent;
