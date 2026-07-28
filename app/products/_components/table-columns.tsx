ed4d20011302380d55e8412c7cddf885b2d3d165
Samuel Heluany <samuelheluany@hotmail.com>
2026-07-28 15:01:44 -0300

    refactor: add ProductTableDropdownMenu component

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -28,6 +28,7 @@ import { Dialog } from "@/app/_components/ui/dialog";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import UpsertProductDialogContent from "./upsert-product-dialog";
 import { useState } from "react";
+import ProductTableDropdownMenu from "./table-dropdown-menu";
 
 const getStatusLabel = (status: string) => {
   if (status === "IN_STOCK") {
@@ -79,58 +80,6 @@ export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "actions",
     header: "Actions",
-    cell: (row) => {
-      const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
-      const product = row.row.original;
-      return (
-        <AlertDialog>
-          <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost">
-                  <MoreHorizontalIcon size={16} />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Ações</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  className="gap-1.5"
-                  // copia o id do produto para o clipboard
-                  onClick={() => {
-                    navigator.clipboard.writeText(product.id);
-                  }}
-                >
-                  <ClipboardCopyIcon size={16} />
-                  Copiar ID
-                </DropdownMenuItem>
-                <DialogTrigger asChild>
-                  <DropdownMenuItem className="gap-1.5">
-                    <EditIcon size={16} />
-                    Editar
-                  </DropdownMenuItem>
-                </DialogTrigger>
-                <AlertDialogTrigger asChild>
-                  <DropdownMenuItem className="gap-1.5">
-                    <TrashIcon size={16} />
-                    Deletar
-                  </DropdownMenuItem>
-                </AlertDialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <UpsertProductDialogContent
-              defaultValues={{
-                id: product.id,
-                name: product.name,
-                price: Number(product.price),
-                stock: product.stock,
-              }}
-              onSuccess={() => setEditDialogIsOpen(false)}
-            />
-            <DeleteProductDialogContent productId={product.id} />
-          </Dialog>
-        </AlertDialog>
-      );
-    },
+    cell: (row) => <ProductTableDropdownMenu product={row.row.original} />,
   },
 ];
