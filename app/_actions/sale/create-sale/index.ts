9a94e3b3d3c8039f8d1eecf8cc0bf446b1e97e99
Samuel Heluany <samuelheluany@hotmail.com>
2026-08-03 07:28:17 -0300

    feat: ensure sale creation is done with a transaction

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -6,42 +6,46 @@ import { revalidatePath } from "next/cache";
 
 export const createSale = async (data: CreateSaleSchema) => {
   createSaleSchema.parse(data);
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
-  });
-  for (const product of data.products) {
-    const productFromDb = await db.product.findUnique({
-      where: {
-        id: product.id,
+  // transactions permite que todas as operações dentro dela sejam executadas. Se uma falhar, todas as outras operações serão revertidas, garantindo a integridade dos dados. Como o find não é uma operação, não precisa mudar para trx.
+  await db.$transaction(async (trx) => {
+    const sale = await trx.sale.create({
+      data: {
+        date: new Date(),
       },
     });
-    if (!productFromDb) {
-      throw new Error("Produto não encontrado.");
-    }
-    const productIsOutOfStock = product.quantity > productFromDb.stock;
-    if (productIsOutOfStock) {
-      throw new Error("Quantidade indisponível no estoque.");
-    }
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
-      },
-    });
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          decrement: product.quantity,
+    for (const product of data.products) {
+      const productFromDb = await db.product.findUnique({
+        where: {
+          id: product.id,
         },
-      },
-    });
-  }
+      });
+      if (!productFromDb) {
+        throw new Error("Produto não encontrado.");
+      }
+      const productIsOutOfStock = product.quantity > productFromDb.stock;
+      if (productIsOutOfStock) {
+        throw new Error("Quantidade indisponível no estoque.");
+      }
+      await trx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
+        },
+      });
+      await trx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+    }
+  });
+
   revalidatePath("/products");
 };
