f6c105464c6daa75deea68ff26a4ba017a690248
Samuel Heluany <samuelheluany@hotmail.com>
2026-08-08 09:44:24 -0300

    feat: add revenue-chart

--- app/_data-access/dashboard/get-dashboard.tsx
+++ app/_data-access/dashboard/get-dashboard.tsx
@@ -1,17 +1,53 @@
 import { db } from "@/app/_lib/prisma";
+import dayjs from "dayjs";
+import "server-only";
 
+export interface DayTotalRevenue {
+  day: string;
+  totalRevenue: number;
+}
 interface DashboardDto {
   totalRevenue: number;
   todayRevenue: number;
   totalSales: number;
   totalStock: number;
   totalProducts: number;
+  totalLast14DaysRevenue: DayTotalRevenue[];
 }
 
 export const getDashboard = async (): Promise<DashboardDto> => {
-  // receita total: todos os produtos vendidos onde soma todos eles pelo preço unitário
+  // pega o dia
+  const today = dayjs().endOf("day").toDate();
+
+  const last14Days = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(
+    (day) => {
+      return dayjs(today).subtract(day, "day");
+    },
+  );
+
+  const totalLast14DaysRevenue: DayTotalRevenue[] = [];
+
+  for (const day of last14Days) {
+    const dayTotalRevenue = await db.$queryRawUnsafe<
+      { totalRevenue: number }[]
+    >(
+      // pega a soma da quantidade * preço unitario
+      `SELECT SUM("unitPrice" * "quantity") as "totalRevenue" FROM "SaleProduct" WHERE "createdAt" >= $1 AND "createdAt" <= $2`,
+      // com o filtro, do começo do dia ate o final do dia
+      day.startOf("day").toDate(),
+      day.endOf("day").toDate(),
+    );
+    // pega o day total revenue e coloca na lista
+    totalLast14DaysRevenue.push({
+      day: day.format("DD/MM"),
+      totalRevenue: dayTotalRevenue[0].totalRevenue,
+    });
+  }
+
+  // receita total: todos os produtos vendidos
   const totalRevenueQuery = `
   SELECT SUM ("unitPrice" * "quantity") as "totalRevenue" from "SaleProduct"`;
+  // receita diaria: todos os produtos vendidos no dia
   const todayRevenueQuery = `
   SELECT SUM ("unitPrice" * "quantity") as "todayRevenue" from "SaleProduct" WHERE "createdAt" >=$1 AND "createdAt" <=$2`;
 
@@ -53,5 +89,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     totalSales,
     totalStock: Number(totalStock._sum.stock),
     totalProducts,
+    totalLast14DaysRevenue,
   };
 };
